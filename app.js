b465448ceee7242c5958637eac217e9896d4945c
Glaubert01 <124006122+Glaubert01@users.noreply.github.com>
2023-10-30 07:28:06 -0300

    Inicio da codificacao javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do numero secreto');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log('Você acertou! O número era ' + chute);
+}
